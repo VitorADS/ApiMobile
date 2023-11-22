bbef6a9397def6552afacc6dc37964695faeb6dc
Vitor Munaretto <vitormunaretto9@gmail.com>
2023-11-22 20:39:18 -0300

    personalizado erro de autenticacao

---
+++ src/EventListener/AuthenticationFailureListener.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\EventListener;
+
+use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationFailureEvent;
+use Lexik\Bundle\JWTAuthenticationBundle\Response\JWTAuthenticationFailureResponse;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class AuthenticationFailureListener
+{
+    /**
+     * @param AuthenticationFailureEvent $event
+     */
+    public function onAuthenticationFailureResponse(AuthenticationFailureEvent $event)
+    {
+        $response = new JWTAuthenticationFailureResponse('Credenciais invalidas', JsonResponse::HTTP_UNAUTHORIZED);
+        $response->setData();
+
+        $event->setResponse($response);
+    }
+}
