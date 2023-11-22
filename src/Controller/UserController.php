6b2dc51fdeb856a5c4301b9304795d436a46a111
Vitor Munaretto <vitormunaretto9@gmail.com>
2023-11-21 23:06:27 -0300

    teste

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -29,7 +29,7 @@ class UserController extends AbstractController
         $this->serializer = new Serializer($normalizers, $encoders);
     }
 
-    #[Route('/api/register', name: 'app_register_user')]
+    #[Route('/register', name: 'app_register_user')]
     public function index(Request $request): JsonResponse
     {
         $user = new User();
