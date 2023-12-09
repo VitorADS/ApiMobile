be36a41f0412514a21cc61874aa9a9c2a858331f
Vitor Munaretto <vitormunaretto9@gmail.com>
2023-12-08 23:57:18 -0300

    Ajuste

--- src/DTO/DespesaDTO.php
+++ src/DTO/DespesaDTO.php
@@ -4,6 +4,7 @@ namespace App\DTO;
 
 use App\Entity\AbstractEntity;
 use App\Entity\User;
+use DateTime;
 use Symfony\Component\Validator\Constraints as Assert;
 
 class DespesaDTO extends AbstractEntity
@@ -12,6 +13,7 @@ class DespesaDTO extends AbstractEntity
         #[Assert\Range(min: 0, max: 1, notInRangeMessage: 'Campo tipo: somente 0 ou 1')]
         public int $tipo = 0,
         public float $valor = 0,
+        public DateTime $data = new DateTime(),
         public ?string $descricao = null,
         public ?User $user = null
     )
